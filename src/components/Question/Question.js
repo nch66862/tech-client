ec9d51de5c8441fddb08a38d30f36c04cbb55dec
Nick Carver <nicholascarver2@outlook.com>
2021-06-24 13:09:34 -0500

    got the edit question button to show up

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,21 +1,32 @@
 import React, { useContext, useEffect, useState } from "react"
 import './Question.css'
 import { QuestionContext } from "./QuestionProvider"
+import { Button } from 'reactstrap';
+
 
 //just a container for a footer for completeness
 export const Question = () => {
     const { getQuestionById } = useContext(QuestionContext)
     const [question, setQuestion] = useState({})
+    const [modal, setModal] = useState(false);
+    const toggleEditQuestion = () => setModal(!modal);
     useEffect(() => {
         getQuestionById(1)
             .then(res => setQuestion(res))
     }, [])
     return (
-        <section className="question">
-            <h2>{question.question?.question_text}</h2>
-            {question.question?.answer_values.map((answer, index) => {
-                return <article key={index}>{answer}</article>
-            })}
-        </section>
+        <>
+            <section className="question">
+                <h2>{question.question?.question_text}</h2>
+                {question.question?.answer_values.map((answer) => {
+                    return (
+                        <article key={answer.id} className="option">
+                            <p>{answer.answer_value}</p>
+                        </article>
+                    )
+                })}
+            </section>
+            <Button color="primary" className="inputTimeButton" onClick={toggleEditQuestion}>Edit</Button>
+        </>
     )
 }
